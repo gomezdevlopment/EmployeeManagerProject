cbb9e16c885bec009cf111a6a8ee7c813e8a57e6
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-08-11 21:28:14 -0400

    Create Employee class

--- src/main/java/com/gomezdevlopment/EmployeeManagerProject/EmployeeManagerProjectApplication.java
+++ src/main/java/com/gomezdevlopment/EmployeeManagerProject/EmployeeManagerProjectApplication.java
@@ -1,5 +1,6 @@
 package com.gomezdevlopment.EmployeeManagerProject;
 
+import com.gomezdevlopment.EmployeeManagerProject.models.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/gomezdevlopment/EmployeeManagerProject/models/Employee.java
@@ -0,0 +1,25 @@
+package com.gomezdevlopment.EmployeeManagerProject.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}
