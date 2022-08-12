6af24650b22f2a71fdf4232bec31fb46deb47161
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-08-11 20:15:49 -0400

    Add project files.

---
+++ src/main/java/com/gomezdevlopment/EmployeeManagerProject/EmployeeManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gomezdevlopment.EmployeeManagerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomezdevlopment/EmployeeManagerProject/EmployeeManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomezdevlopment.EmployeeManagerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
