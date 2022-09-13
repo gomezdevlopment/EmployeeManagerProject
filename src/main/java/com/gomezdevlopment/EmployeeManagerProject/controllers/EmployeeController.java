22bf7c854d7ce9df1b66339478861d4949975380
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-13 16:48:26 -0400

    Add CrossOrigin annotation for the employee controller class

--- src/main/java/com/gomezdevlopment/EmployeeManagerProject/controllers/EmployeeController.java
+++ src/main/java/com/gomezdevlopment/EmployeeManagerProject/controllers/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
